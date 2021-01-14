fe60c1b2f8c348befbc56bc821f7353c08ee8a75
Akrobate <fedorov.artiom@gmail.com>
2021-01-14 03:12:59 +0100

    Enriched RssParser methods

--- src/libraries/RssFeedParser.js
+++ src/libraries/RssFeedParser.js
@@ -27,10 +27,34 @@ class RssFeedParser {
      * @param {String} url
      * @returns {Promise<Object>}
      */
+    parseRssFeedUrlWithUrlCheck(url) {
+        return new Promise((resolve, reject) => {
+            if (!url) {
+                return reject(new Error('INVALID_PARAM_URL'));
+            }
+            return this.parseRssFeedUrl(url)
+                .then(resolve)
+                .catch(reject);
+        });
+    }
+
+    /**
+     * @param {String} url
+     * @returns {Promise<Object>}
+     */
     parseRssFeedUrl(url) {
         return this.parser
             .parseURL(url);
     }
+
+    /**
+     * @param {*} website_url
+     * @returns {String}
+     */
+    generateDefaultFeedUrlFromWebsiteUrl(website_url) {
+        return `${website_url}/feed`.replace('//feed', '/feed');
+    }
+
 }
 
 RssFeedParser.instance = null;
