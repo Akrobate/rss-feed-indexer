9e19a7e67b88aea72a404b8838048a72dce98c51
Akrobate <fedorov.artiom@gmail.com>
2021-01-17 23:42:39 +0100

    Added optimised script for downloading rss feeds

---
+++ src/scripts/process-optimised-rss-items-download.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// 55376
+
+const Promise = require('bluebird');
+
+const {
+    logger,
+} = require('../logger');
+
+const {
+    FeedItemsDownloader,
+} = require('../libraries/');
+
+const {
+    MongoDbRepository,
+} = require('../repositories/');
+
+const mongodb_repository = MongoDbRepository.getInstance();
+const feed_items_downloader = FeedItemsDownloader.getInstance();
+const RSS_FEED_URL_COLLECTION_NAME = 'rss-feed-url';
+
+mongodb_repository
+    .findDocumentList(
+        RSS_FEED_URL_COLLECTION_NAME,
+        {
+            rss_available: true,
+        }
+    )
+    .then((url_row_list) => {
+        logger.log('Documents loaded');
+        return Promise.mapSeries(url_row_list, (url_row) => {
+            logger.log(`Processing - ${url_row.rss_feed_url}`);
+            return feed_items_downloader
+                .getItemsFromRssFeedUrl(url_row.rss_feed_url, url_row.id)
+                .then(() => {
+                    logger.info('Inserted');
+                })
+                .catch((error) => {
+                    logger.info('Error:', error);
+                });
+        });
+    })
+    .finally(() => {
+        console.log('Exiting process');
+        mongodb_repository.closeConnection();
+        // eslint-disable-next-line no-process-exit
+        process.exit();
+    });
