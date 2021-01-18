c598aac39f167b95d2e408757629534095106803
Akrobate <fedorov.artiom@gmail.com>
2021-01-18 13:54:47 +0100

    Added concurrency to the optimised feed script

--- src/scripts/process-optimised-rss-items-download.js
+++ src/scripts/process-optimised-rss-items-download.js
@@ -16,6 +16,8 @@ const {
     MongoDbRepository,
 } = require('../repositories/');
 
+const CONCURRENCY = 50;
+
 const mongodb_repository = MongoDbRepository.getInstance();
 const feed_items_downloader = FeedItemsDownloader.getInstance();
 const RSS_FEED_URL_COLLECTION_NAME = 'rss-feed-url';
@@ -29,17 +31,23 @@ mongodb_repository
     )
     .then((url_row_list) => {
         logger.log('Documents loaded');
-        return Promise.mapSeries(url_row_list, (url_row) => {
-            logger.log(`Processing - ${url_row.rss_feed_url}`);
-            return feed_items_downloader
-                .getItemsFromRssFeedUrl(url_row.rss_feed_url, url_row.id)
-                .then(() => {
-                    logger.info('Inserted');
-                })
-                .catch((error) => {
-                    logger.info('Error:', error);
-                });
-        });
+        return Promise.map(
+            url_row_list,
+            (url_row) => {
+                logger.log(`Processing - ${url_row.rss_feed_url}`);
+                return feed_items_downloader
+                    .getItemsFromRssFeedUrl(url_row.rss_feed_url, url_row.id)
+                    .then(() => {
+                        logger.info('Inserted');
+                    })
+                    .catch((error) => {
+                        logger.info('Error:', error);
+                    });
+            },
+            {
+                concurrency: CONCURRENCY,
+            }
+        );
     })
     .finally(() => {
         console.log('Exiting process');
