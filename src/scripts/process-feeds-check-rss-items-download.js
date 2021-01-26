cf9be8df647cd1f81f283c198f7b857f5d5a2f71
Akrobate <fedorov.artiom@gmail.com>
2021-01-26 13:17:56 +0100

    Cleaned feeds check script

--- src/scripts/process-feeds-check-rss-items-download.js
+++ src/scripts/process-feeds-check-rss-items-download.js
@@ -16,11 +16,13 @@ const {
     MongoDbRepository,
 } = require('../repositories/');
 
+const CONCURRENCY = 20;
+
 const mongodb_repository = MongoDbRepository.getInstance();
 const feed_items_downloader = FeedItemsDownloader.getInstance();
 const rss_feed_url_collection_name = 'rss-feed-url';
 
-function updateUrl(data, _id) {
+function updateUrl(_id, data) {
     return mongodb_repository.updateDocument(
         rss_feed_url_collection_name,
         {
@@ -34,24 +36,35 @@ function updateUrl(data, _id) {
 
 mongodb_repository.findDocumentList(rss_feed_url_collection_name, {})
     .then((url_row_list) => {
-        logger.log('Document list');
-        return Promise.mapSeries(url_row_list, (url_row) => {
-            logger.log(`Processing - ${url_row.rss_feed_url}`);
-            return feed_items_downloader
-                .getItemsFromRssFeedUrl(url_row.rss_feed_url, url_row.id)
-                .then(() => {
-                    logger.log('Added');
-                    return updateUrl({
-                        rss_available: true,
-                    }, url_row._id);
-                })
-                .catch((error) => {
-                    logger.log(`Error ${error.message}`);
-                    return updateUrl({
-                        rss_available: false,
-                    }, url_row._id);
-                });
-        });
+        logger.log('Documents loaded');
+        return Promise.map(
+            url_row_list,
+            (url_row) => {
+                logger.log(`Processing - ${url_row.rss_feed_url}`);
+                return feed_items_downloader
+                    .getItemsFromRssFeedUrl(url_row.rss_feed_url, url_row.id)
+                    .then(() => {
+                        logger.log('Added');
+                        return updateUrl(
+                            url_row._id,
+                            {
+                                rss_available: true,
+                            }
+                        );
+                    })
+                    .catch((error) => {
+                        logger.log(`Error ${error.message}`);
+                        return updateUrl(
+                            url_row._id,
+                            {
+                                rss_available: false,
+                            }
+                        );
+                    });
+            },
+            {
+                concurrency: CONCURRENCY,
+            });
     })
     .finally(() => {
         console.log('Exiting process');
