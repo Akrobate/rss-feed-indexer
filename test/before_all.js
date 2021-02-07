c29e983dc0c43f9560980fcea5a5cda989a37b11
Akrobate <fedorov.artiom@gmail.com>
2021-02-07 22:13:58 +0100

    Added drop test database before tests starts

--- test/before_all.js
+++ test/before_all.js
@@ -8,11 +8,23 @@ const {
     configuration,
 } = require('../src/configuration');
 
+const {
+    MongoDbRepository,
+} = require('../src/repositories/MongoDbRepository');
+
 const stubs = {};
 
 before((done) => {
-    stubs.database_name = stub(configuration.storage.mongodb, 'database_name').value('test-database');
 
-    console.log('............... Before all, setting up configuration ..............');
-    done();
+    const test_database_name = 'test-database';
+    stubs.database_name = stub(configuration.storage.mongodb, 'database_name').value(test_database_name);
+
+    MongoDbRepository
+        .getInstance()
+        .getConnection()
+        .then((connection) => connection.db(test_database_name))
+        .then((dbo) => dbo.dropDatabase(() => {
+            console.log('............... Before all, setting up configuration ..............');
+            done();
+        }));
 });
