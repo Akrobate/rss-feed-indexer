e99724eae331297b3a6d1a22fbee634faff83e3b
Akrobate <fedorov.artiom@gmail.com>
2021-02-08 13:48:46 +0100

    Added test count

--- test/controllers/RssFeedItemController.test.js
+++ test/controllers/RssFeedItemController.test.js
@@ -11,6 +11,9 @@ const {
     RssFeedItemService,
 } = require('../../src/services');
 const {
+    RssFeedItemRepository,
+} = require('../../src/repositories');
+const {
     app,
 } = require('../../src/server-app');
 
@@ -20,10 +23,12 @@ describe('RssFeedItemController', () => {
 
     beforeEach(() => {
         mocks.rss_feed_item_service = mock(RssFeedItemService.getInstance());
+        mocks.rss_feed_item_repository = mock(RssFeedItemRepository.getInstance());
     });
 
     afterEach(() => {
         mocks.rss_feed_item_service.restore();
+        mocks.rss_feed_item_repository.restore();
     });
 
     it('Should be able to ping server', (done) => {
@@ -104,6 +109,41 @@ describe('RssFeedItemController', () => {
                 expect(response.body).to.deep.equal({
                     count: 0,
                     rss_item_list: [],
+                });
+                done();
+            });
+    });
+
+    it('Should be able to count', (done) => {
+
+        mocks.rss_feed_item_repository
+            .expects('countDailyAggregated')
+            .once()
+            .withArgs({
+                company_id_list: undefined,
+                is_visible: true,
+                language_list: ['french'],
+                publication_end_date: '2020-12-31T00:00:00Z',
+                publication_start_date: '2020-12-31T00:00:00Z',
+            }, false)
+            .returns(Promise.resolve(12));
+
+        superTest(app)
+            .post('/api/rss-feed-items/count')
+            .send({
+                publication_end_date: '2020-12-31T00:00:00Z',
+                publication_start_date: '2020-12-31T00:00:00Z',
+            })
+            // .expect(200)
+            .end((error, response) => {
+
+                mocks.rss_feed_item_service.verify();
+
+                if (error) {
+                    done(error);
+                }
+                expect(response.body).to.deep.equal({
+                    rss_item_count_list: 12,
                 });
                 done();
             });
