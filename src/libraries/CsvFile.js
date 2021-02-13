3ab79095299ddbf578ff1327bb63d589863b929b
Akrobate <fedorov.artiom@gmail.com>
2021-02-13 22:17:12 +0100

    Moving script code to csvfile code to keep script sckinny (WIP)

--- src/libraries/CsvFile.js
+++ src/libraries/CsvFile.js
@@ -2,7 +2,9 @@
 
 const fs = require('fs');
 const csv = require('csv-parser');
-
+const {
+    MongoDbRepository,
+} = require('../repositories');
 class CsvFile {
 
     /**
@@ -10,9 +12,15 @@ class CsvFile {
      */
     static getInstance() {
         if (CsvFile.instance === null) {
-            CsvFile.instance = new CsvFile();
+            CsvFile.instance = new CsvFile(
+                MongoDbRepository.getInstance()
+            );
         }
         return CsvFile.instance;
+    }
+
+    constructor(mongo_db_repository) {
+        this.mongo_db_repository = mongo_db_repository;
     }
 
     /**
@@ -29,6 +37,50 @@ class CsvFile {
             .on('error', (error) => reject(error))
             .on('end', () => resolve())
         );
+    }
+
+
+    /**
+     * @returns {Promise}
+     */
+    loadCsvFileUrlToDatabase() {
+        return this.mongodb_repository
+            .createCollectionIfNotExists(rss_feed_url_collection_name)
+            .then(() => csv_file.readLinePerLineCsvFile(source_file, (data) => {
+                website_url_to_insert.push(data);
+                website_url_to_insert_count += 1;
+            }))
+            .then(() => {
+                logger.log('Start inserting');
+                return Promise.mapSeries(
+                    website_url_to_insert,
+                    (data) => {
+                        logger.log(`Remaining documents to insert: ${website_url_to_insert_count}`);
+                        website_url_to_insert_count--;
+                        return mongodb_repository
+                            .findDocument(rss_feed_url_collection_name, {
+                                website_url: data.website_url,
+                            })
+                            .then((found_document) => {
+                                if (found_document === null) {
+                                    return mongodb_repository
+                                        .insertDocument(rss_feed_url_collection_name,
+                                            {
+                                                id: Number(data.id),
+                                                website_url: data.website_url,
+                                                rss_feed_url: `${data.website_url}/feed`.replace('//feed', '/feed'),
+                                                last_check_date: null,
+                                                rss_available: null,
+                                            }
+                                        )
+                                        .catch(logger.log);
+                                }
+                                return null;
+                            });
+                    }
+                );
+            })
+            .then(() => mongodb_repository.closeConnection());
     }
 }
 
