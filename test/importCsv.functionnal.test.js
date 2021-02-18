2ce25d89d207571ed11c76344e7819fc5e5a0be7
Akrobate <fedorov.artiom@gmail.com>
2021-02-18 13:29:55 +0100

    Added import csv test

---
+++ test/importCsv.functionnal.test.js
@@ -0,0 +1,13 @@
+const {
+    CsvFileLoader,
+} = require('../src/librairies');
+
+const csv_file_loader = CsvFileLoader.getInstance();
+const seed_path = './test/seeds/feed_rss_result';
+
+
+describe('CSV Import functionnal test', () => {
+    it('Should be able to import csv file', (done) => {
+        done();
+    });
+});
