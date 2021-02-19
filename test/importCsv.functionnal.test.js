917002df84aa55c8a2f598ed2205ee5af12e503f
Akrobate <fedorov.artiom@gmail.com>
2021-02-19 18:50:00 +0100

    Testing import script

--- test/importCsv.functionnal.test.js
+++ test/importCsv.functionnal.test.js
@@ -1,13 +1,16 @@
 const {
     CsvFileLoader,
-} = require('../src/librairies');
+} = require('../src/libraries');
 
 const csv_file_loader = CsvFileLoader.getInstance();
-const seed_path = './test/seeds/feed_rss_result';
+const seed_path = './test/seeds/url_source.csv';
 
 
 describe('CSV Import functionnal test', () => {
     it('Should be able to import csv file', (done) => {
-        done();
+        csv_file_loader
+            .loadCsvFileUrlToDatabase(seed_path)
+            .then(() => done())
+            .catch(done);
     });
 });
