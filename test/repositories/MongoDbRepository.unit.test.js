2d10c84afe8c847a916ea4dadba00f3a1ec0052d
Akrobate <fedorov.artiom@gmail.com>
2021-02-22 12:47:21 +0100

    Added test to mongodb repository

--- test/repositories/MongoDbRepository.unit.test.js
+++ test/repositories/MongoDbRepository.unit.test.js
@@ -1,15 +1,11 @@
 'use strict';
 
 const {
-    RssFeedItemRepository,
-} = require('../../src/repositories');
-
-const {
     expect,
 } = require('chai');
 const {
     MongoDbRepository,
-} = require('../src/repositories/MongoDbRepository');
+} = require('../../src/repositories/MongoDbRepository');
 const mongo_db_repository = MongoDbRepository.getInstance();
 
 const test_collection = 'test-collection-unit-tests';
@@ -20,11 +16,52 @@ describe('MongoDbRepository unit test', function() {
     // eslint-disable-next-line no-invalid-this
     this.timeout(5000);
 
-    it('Should be able to createCollectionIfNotExists', (done) => {
+    describe('Test suite create / delete collection', () => {
 
-        mongo_db_repository.createCollectionIfNotExists(test_collection)
-            .then(() => done())
-            .catch(done);
+        it('Should be able to createCollectionIfNotExists (when not exists)', (done) => {
+            mongo_db_repository.createCollectionIfNotExists(test_collection)
+                .then(() => Promise
+                    .resolve(
+                        mongo_db_repository.useDataBase(MongoDbRepository.DATABASE_NAME)
+                    )
+                    .then((dbo) => dbo.collections())
+                    .then((collection_list) => {
+                        if (!collection_list.map((colection) => colection.s.namespace.collection)
+                            .includes(test_collection)) {
+                            return done(new Error('Collection was not created'));
+                        }
+                        return done();
+                    })
+                )
+                .catch(done);
+        });
+
+        it('Should be able to createCollectionIfNotExists (when exists)', (done) => {
+            mongo_db_repository.createCollectionIfNotExists(test_collection)
+                .then((result) => {
+                    expect(result).to.equal(true);
+                    done();
+                })
+                .catch(done);
+        });
+
+        it('Should be able to dropCollection', (done) => {
+            mongo_db_repository.dropCollection(test_collection)
+                .then(() => Promise
+                    .resolve(
+                        mongo_db_repository.useDataBase(MongoDbRepository.DATABASE_NAME)
+                    )
+                    .then((dbo) => dbo.collections())
+                    .then((collection_list) => {
+                        if (!collection_list.map((colection) => colection.s.namespace.collection)
+                            .includes(test_collection)) {
+                            return done();
+                        }
+                        return done('Collection wan not removed');
+                    })
+                )
+                .catch(done);
+        });
     });
 
 });
