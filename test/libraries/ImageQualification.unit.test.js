42c2ccde4a06cbd7201b17cac95b956759f7cce2
Akrobate <fedorov.artiom@gmail.com>
2021-02-23 13:56:29 +0100

    Added ImageQualification test scaffold

---
+++ test/libraries/ImageQualification.unit.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+describe('ImageQualification unit tests', () => {
+
+    it('Should be ablse to extract multiplce extractImgUrlsFromHtml', (done) => {
+        done();
+    });
+});
