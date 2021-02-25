5b8d192ecff1db76f1ec9bceaf952497a67b490f
Akrobate <fedorov.artiom@gmail.com>
2021-02-25 14:03:59 +0100

    Finished mocks

--- test/libraries/ImageQualification.unit.test.js
+++ test/libraries/ImageQualification.unit.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const {
-    ImageQualification,
-} = require('../src/librairies');
+    ImagesQualification,
+} = require('../../src/libraries');
 const axios = require('axios');
 const {
     mock,
@@ -11,12 +11,12 @@ const {
     expect,
 } = require('chai');
 
-describe('ImageQualification unit tests', () => {
+describe.only('ImagesQualification unit tests', () => {
     const mocks = {};
     let image_qualification = null;
 
     before((done) => {
-        image_qualification = ImageQualification.getInstance();
+        image_qualification = ImagesQualification.getInstance();
         mocks.axios = mock(axios);
         done();
     });
@@ -27,24 +27,63 @@ describe('ImageQualification unit tests', () => {
         done();
     });
 
+
     it('Should be ablse to extract multiplce extractImgUrlsFromHtml', (done) => {
         image_qualification.setSizeThreshold(1000);
         image_qualification.setDetectionTimeout(1);
 
+        const url_list = [
+            'http://www.domain.com/url1.jpg',
+            'http://www.domain.com/url2.jpg',
+            'http://www.domain.com/url3.jpg',
+        ];
+
         mocks.axios.expects('head')
-            .withArgs(image_url,
+            .once()
+            .withArgs(
+                url_list[0],
                 {
                     timeout: 1,
                 }
             )
-            .returns({
+            .returns(Promise.resolve({
+                size: 500,
+                url: url_list[0],
+            }));
+
+        mocks.axios.expects('head')
+            .once()
+            .withArgs(
+                url_list[1],
+                {
+                    timeout: 1,
+                }
+            )
+            .returns(Promise.resolve({
+                size: 2000,
+                url: url_list[1],
+            }));
+        mocks.axios.expects('head')
+            .once()
+            .withArgs(
+                url_list[2],
+                {
+                    timeout: 1,
+                }
+            )
+            .returns(Promise.resolve({
+                size: 3000,
+                url: url_list[2],
+            }));
+
+        image_qualification
+            .findBestImage(url_list)
+            .then((data) => {
+                mocks.axios.verify();
+                console.log(data);
+                done();
 
             });
-        
-            
-        
-        done();
-
 
     });
 });
