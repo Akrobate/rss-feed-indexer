5d4f49f7f12c9b5272d84ad6a324ee47a9334f02
Akrobate <fedorov.artiom@gmail.com>
2021-03-05 13:59:37 +0100

    Enrich RssFeedItem repository

--- test/repositories/RssFeedItemRepository.unit.test.js
+++ test/repositories/RssFeedItemRepository.unit.test.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const {
+    MongoDbRepository,
     RssFeedItemRepository,
 } = require('../../src/repositories');
-
+const {
+    mock,
+} = require('sinon');
 const {
     expect,
 } = require('chai');
@@ -193,5 +196,144 @@ describe('RssFeedItemRepository', function() {
             done();
         });
 
+        it('formating language_list', (done) => {
+            const params = {
+                language_list: ['French'],
+            };
+
+            const rss_feed_item_repository = RssFeedItemRepository.getInstance();
+            const result = rss_feed_item_repository.formatSearchCriteria(params);
+            expect(result).to.deep.equal({
+                language: {
+                    $in: params.language_list,
+                },
+            });
+            done();
+        });
+        it('formating company_id_list', (done) => {
+            const params = {
+                company_id_list: [110, 120],
+            };
+
+            const rss_feed_item_repository = RssFeedItemRepository.getInstance();
+            const result = rss_feed_item_repository.formatSearchCriteria(params);
+            expect(result).to.deep.equal({
+                rss_feed_url_id: {
+                    $in: params.company_id_list,
+                },
+            });
+            done();
+        });
+        it('formating is_visible ', (done) => {
+            const params = {
+                is_visible: true,
+            };
+            const rss_feed_item_repository = RssFeedItemRepository.getInstance();
+            const result = rss_feed_item_repository.formatSearchCriteria(params);
+            expect(result).to.deep.equal({
+                is_visible: params.is_visible,
+            });
+            done();
+        });
+    });
+
+    describe('Unit testing', () => {
+
+        const mocks = {};
+        const mongo_db_repository = MongoDbRepository.getInstance();
+        const rss_feed_item_repository = RssFeedItemRepository.getInstance();
+
+        beforeEach((done) => {
+            mocks.mongo_db_repository = mock(mongo_db_repository);
+            mocks.rss_feed_item_repository = mock(rss_feed_item_repository);
+            done();
+        });
+
+        afterEach((done) => {
+            mocks.mongo_db_repository.restore();
+            mocks.rss_feed_item_repository.restore();
+            done();
+        });
+
+        it('search', (done) => {
+            const params = {
+                limit: 10,
+                offset: 10,
+                publication_start_date: new Date('Thu Apr 16 2020 09:23:32 GMT+0000'),
+            };
+            mocks.mongo_db_repository.expects('findDocumentList')
+                .once()
+                .withArgs(
+                    RssFeedItemRepository.RSS_FEED_ITEMS_COLLECTION_NAME,
+                    {
+                        pubDate: {
+                            $lte: params.publication_start_date,
+                        },
+                    },
+                    params.limit,
+                    params.offset,
+                    undefined,
+                    {
+                        pubDate: -1,
+                    }
+                )
+                .returns(Promise.resolve({}));
+            RssFeedItemRepository.getInstance()
+                .search(params)
+                .then(() => {
+                    mocks.mongo_db_repository.verify();
+                    done();
+                })
+                .catch(done);
+        });
+        it('closeConnection', (done) => {
+            mocks.mongo_db_repository.expects('closeConnection')
+                .once()
+                .returns(Promise.resolve({}));
+            RssFeedItemRepository
+                .getInstance()
+                .closeConnection()
+                .then(() => {
+                    mocks.mongo_db_repository.verify();
+                    done();
+                })
+                .catch(done);
+        });
+        it('countDailyAggregated', (done) => {
+            mocks.rss_feed_item_repository.expects('countDateAggregated')
+                .withArgs(
+                    {},
+                    '%Y-%m-%d',
+                    true
+                )
+                .once()
+                .returns(Promise.resolve({}));
+            RssFeedItemRepository.getInstance()
+                .countDailyAggregated({}, true)
+                .then(() => {
+                    mocks.rss_feed_item_repository.verify();
+                    done();
+                })
+                .catch(done);
+        });
+        it('createIndexForExistanceCheck', (done) => {
+            mocks.mongo_db_repository.expects('createIndex')
+                .withArgs(
+                    RssFeedItemRepository.RSS_FEED_ITEMS_COLLECTION_NAME,
+                    {
+                        guid: 1,
+                        rss_feed_url_id: 1,
+                    }
+                )
+                .once()
+                .returns(Promise.resolve({}));
+            RssFeedItemRepository.getInstance()
+                .createIndexForExistanceCheck()
+                .then(() => {
+                    mocks.mongo_db_repository.verify();
+                    done();
+                })
+                .catch(done);
+        });
     });
 });
