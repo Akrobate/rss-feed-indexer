282f4b331a70ef0776ea150fc57f7861ff3d0daf
Akrobate <fedorov.artiom@gmail.com>
2022-03-14 00:41:19 +0100

    Logger info fixed

--- src/scripts/source-insert-url-rss-feeds.js
+++ src/scripts/source-insert-url-rss-feeds.js
@@ -10,10 +10,10 @@ const {
     CsvFileLoader,
 } = require('../libraries');
 
-const source_file = '../data/sources/WordPress.csv';
+const source_file = `${__dirname}/../../data/sources/companies_site_web-13-03-2022.csv`;
 const csv_file_loader = CsvFileLoader.getInstance();
 
 logger.log('Start importing url');
 csv_file_loader
     .loadCsvFileUrlToDatabase(source_file)
-    .then(() => logger('Importing finished'));
+    .then(() => logger.info('Importing finished'));
